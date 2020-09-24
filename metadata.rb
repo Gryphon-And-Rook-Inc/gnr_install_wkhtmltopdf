51134c64d078e138d1d388e6483ecf36d4f4a47d
gryphonandrook <38541934+gryphonandrook@users.noreply.github.com>
2020-09-23 22:28:05 -0600

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name              'gnr_install_wkhtmltopdf'
+name              'gnr_server_tools'
 maintainer        'Gryphon & Rook Inc'
 license           'All rights reserved'
-description       'Install wkhtmltopdf 0.12.2.1'
+description       'Install Customized Server Tools'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.1.0'
+version           '0.1.1'
