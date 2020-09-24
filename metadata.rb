044907389ea1991ff2771f081666ce88a6e875c0
gryphonandrook <38541934+gryphonandrook@users.noreply.github.com>
2020-09-23 21:01:42 -0600

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name              'gnr_install_wkhtmltopdf_0.12.2.1'
+maintainer        'Gryphon & Rook Inc'
+license           'All rights reserved'
+description       'Install wkhtmltopdf 0.12.2.1'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '0.1.0'
